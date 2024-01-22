eb3bb60631d14b858a4683b468571f70140be8aa
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-01-22 12:56:22 +0800

    passed test 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  for (let i = 0; i <= num; i++) {
+    if (i === num) return string.repeat(i);
+  }
+  if (num < 0) return "ERROR";
 };
 
 // Do not edit below this line
