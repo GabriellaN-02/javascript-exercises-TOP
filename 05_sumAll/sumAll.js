59d2de8b81115afb886e5fcfbfe438e1da4d11d4
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-01-23 12:19:05 +0800

    passed test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  let sum = 0;
+  let bigger = startNum;
 
+  // ERROR if negative numbers or not numbers
+  if (startNum < 0 || endNum < 0) return "ERROR";
+  if (typeof startNum !== "number" || typeof endNum !== "number") return "ERROR";
+
+  if (endNum < bigger) {
+    for (let i = endNum; i <= bigger; i++) sum += i;
+  } else {
+    for (let i = bigger; i <= endNum; i++) sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
