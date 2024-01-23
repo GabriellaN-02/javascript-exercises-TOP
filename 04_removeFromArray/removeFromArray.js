c6433d936df1b40b3b8cb44960ffebe181c635bb
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-01-23 11:25:33 +0800

    passed test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  let newArr = [];
 
+  array.forEach((element) => {
+    if (!args.includes(element)) {
+      newArr.push(element);
+    }
+  });
+
+  return newArr;
 };
 
 // Do not edit below this line
