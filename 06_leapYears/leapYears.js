946992ed41472ab16a588453065d847e5e001aed
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-01-23 12:41:50 +0800

    passed test 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // true if divisible by 100 and 400
+  if (year % 100 === 0 && year % 400 === 0) return true;
 
+  // false if divisible by 100
+  if (year % 100 === 0) return false;
+
+  // true if divisible by 4
+  if (year % 4 === 0) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
